37dab23b36afd57247dab3c7afd1dd0594b71659
Marconi-Gomes <marconegomes100@gmail.com>
2024-10-26 22:59:51 -0300

    Update login.js

--- login.js
+++ login.js
@@ -39,6 +39,7 @@ function login() {
         message.style.color = "red";
         message.textContent = "Usuário ou senha inválidos.";
     }
+}
 // login.js
 function login() {
     const username = document.getElementById("username").value.trim();
@@ -46,7 +47,7 @@ function login() {
     const message = document.getElementById("message");
 
     // Credenciais de exemplo
-    const validUsername = "ana";
+    const validUsername = "visitante";
     const validPassword = "123";
     
     
@@ -60,4 +61,26 @@ function login() {
         message.textContent = "Usuário ou senha inválidos.";
     }
 }
+// login.js
+function login() {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const message = document.getElementById("message");
+
+    // Credenciais de exemplo
+    const validUsername = "myharakiri";
+    const validPassword = "123";
+    
+    
+
+    // Verifica se as credenciais são válidas
+    if (username === validUsername && password === validPassword) {
+        localStorage.setItem("isLoggedIn", "true"); // Armazena a sessão
+        window.location.href = "index.html"; // Redireciona para a página principal
+    } else {
+        message.style.color = "red";
+        message.textContent = "Usuário ou senha inválidos.";
+    }
+}
+
 
